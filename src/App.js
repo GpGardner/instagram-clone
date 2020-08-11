1f2b7f5fc2577e1e8a002f873e05c0efcc516636
George Gardner <gpgardner@yahoo.com>
2020-08-11 15:25:32 -0400

    adding post, avatar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
+import Post from './Post';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+
+    {/* using bem convention */}
+    <div className="app__header">
+      <img className="app__headerImage" 
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt="insta logo"/>   
+    </div>
+      {/* Header */}
+      {/* Posts */}
+      <Post />
+      {/* Posts */}
+
     </div>
   );
 }
