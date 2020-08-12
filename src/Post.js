2be720304fce99f1e8dfbec62afbdd6a21ae84f4
George Gardner <gpgardner@yahoo.com>
2020-08-12 15:25:18 -0400

    Beginning to accept comments

--- src/Post.js
+++ src/Post.js
@@ -1,27 +1,68 @@
-import React from 'react'
-import "./Post.css"
-import Avatar from "@material-ui/core/Avatar"
+import React, { useState, useEffect } from "react";
+import "./Post.css";
+import { Input, Avatar, Button } from "@material-ui/core";
+import { db } from "./firebase";
 
 function Post(props) {
-	const { imageUrl, username, caption } = props 
-	return (
-		
-		<div className="post">
-			<div className="post__header">
-				<Avatar className="post__avatar" alt={username} src="/static/images/avatar/1.jpg" />
-				<h3 className="post__username">{username}</h3>
-			</div>
-			{/* header -> avatar + username */}
+  const { imageUrl, username, caption, postId } = props;
 
-			<img className="post__image" src={imageUrl}
-				alt=""/>
-			{/* image */}
-			{/* like comment  */}
-			<h4 className="post__text"><span className="post__username">{username}: </span> {caption}</h4>
-			{/* username + caption */}
+  const [comments, setComments] = useState({});
+  const [addComment, setAddComment] = useState("");
 
-		</div>
-	)
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection("posts")
+        .doc(postId)
+        .collection("comments")
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
+    return () => {
+      unsubscribe();
+    };
+  }, [postId]);
+
+  const postCommet = (e) => {};
+
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt={username}
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3 className="post__username">{username}</h3>
+      </div>
+      {/* header -> avatar + username */}
+
+      <img className="post__image" src={imageUrl} alt="" />
+      {/* image */}
+      {/* like comment  */}
+      <h4 className="post__text">
+        <span className="post__username">{username}: </span> {caption}
+      </h4>
+      {/* username + caption */}
+
+      <form className="post__commentBar">
+        <Input
+          className="post__input"
+          placeholder="Add Comment..."
+          onChange={(e) => setAddComment(e.target.value)}
+        />
+        {addComment ? (
+          <Button className="post__button">Post</Button>
+        ) : (
+          <Button className="post__button" disabled>
+            Post
+          </Button>
+        )}
+      </form>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
