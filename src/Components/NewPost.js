7c5dd47d6e491731e43a07c164003d2b20f3c01a
George Gardner <gpgardner@yahoo.com>
2020-08-12 13:06:29 -0400

    NewPost modal

---
+++ src/Components/NewPost.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import { Modal, Input, Button } from "@material-ui/core"
+import { storage, db } from "../firebase"
+import firebase from "firebase" 
+
+function NewPost(props) {
+
+	const {
+    modalStyle,
+		classes,
+		username,
+		openNewPost,
+		setOpenNewPost,
+  } = props;
+
+	const [caption, setCaption] = useState('');
+	const [image, setImage] = useState(null);
+	const [progress, setProgress] = useState('');
+
+	const handleChange = (e) => {
+		if(e.target.files[0]) {
+			setImage(e.target.files[0]);
+		}
+	}
+
+	const handleSubmitPost = (e) => {
+		e.preventDefault();
+		const uploadTask = storage.ref(`images/${image.name}`).put(image)
+
+		uploadTask.on("state_changed", (snapshot) => {
+			//progress function ...
+			const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+			setProgress(progress);
+			},
+			(error) => {
+				console.error(error);
+			},
+			() => {
+				storage
+					.ref("images")
+					.child(image.name)
+					.getDownloadURL()
+					.then(url => {
+						//posting image in db
+						db.collection("posts").add({
+							timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+							caption: caption,
+							imageUrl: url,
+							username: username
+						});
+						setProgress(0);
+						setCaption('');
+						setImage(null);
+						setOpenNewPost(false);
+					})
+			}
+		)
+  };
+
+	return (
+		<div>
+			{console.log(username)}
+			<Modal open={openNewPost} onClose={() => setOpenNewPost(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt="insta logo"
+              />
+							<progress value={progress} max="100"/>
+            </center>
+						<Input type="file" onChange={handleChange}/>
+						<Input
+              placeholder="Enter a caption"
+              type="text"
+              value={caption}
+              onChange={(e) => setCaption(e.target.value)}
+            />
+            <Button type="submit" onClick={handleSubmitPost}>
+              Submit
+            </Button>
+          </form>
+        </div>
+      </Modal>
+		</div>
+	)
+}
+
+export default NewPost
