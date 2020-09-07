14e1c85714f98310f51fe08217730b648334db66
George Gardner <gpgardner@yahoo.com>
2020-09-06 20:18:28 -0400

    passing likes through props

--- src/App.js
+++ src/App.js
@@ -145,6 +145,7 @@ function App() {
             username={post.username}
             caption={post.caption}
             imageUrl={post.imageUrl}
+            likes={post.likes}
           />
         ))}
       </center>
